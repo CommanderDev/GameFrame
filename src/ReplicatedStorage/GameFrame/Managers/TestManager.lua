44f47c272ab972db8b2e0317a34d57ab7c00febe
CommanderDev <57833245+CommanderDev@users.noreply.github.com>
2021-11-16 18:20:27 -0500

    Added missing depednencies

--- src/ReplicatedStorage/GameFrame/Managers/TestManager.lua
+++ src/ReplicatedStorage/GameFrame/Managers/TestManager.lua
@@ -2,8 +2,4 @@ local GameFrame = require(game.ReplicatedStorage.GameFrame)
 
 local TestManager = GameFrame.createManager {Name = script.Name}
 
-function TestManager:init()
-    print(self)
-end
-
 return TestManager
